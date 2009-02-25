5e4682e633f031dff5d83a0f624d69acec729c6f
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-02-25 14:52:51 +0000

    rename outputs as ttl files.

--- biordf2009_query_federation_case/tcm-data/transfer-effect-to-ntriple.py
+++ biordf2009_query_federation_case/tcm-data/transfer-effect-to-ntriple.py
@@ -11,8 +11,8 @@ import logging
 infilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_effect_associations.tab'
 infile = codecs.open(infilename, mode='r', encoding='UTF-8')
 
-#outfilename = '/root/workspace/biordf2009_query_federation_case/tcm-data/TCM_effect_associations.nt'
-outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_effect_associations.nt'
+#outfilename = '/root/workspace/biordf2009_query_federation_case/tcm-data/TCM_effect_associations.ttl'
+outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_effect_associations.ttl'
 outfile = codecs.open(outfilename, mode='w', encoding='UTF-8')
 
 ingredientname = '<http://tcm.lifescience.ntu.edu.tw/id/ingredient/'
@@ -20,6 +20,7 @@ effectname = '<http://tcm.lifescience.ntu.edu.tw/id/effect/'
 type_ingredient = '<http://tcm.lifescience.ntu.edu.tw/Ingredient>'
 type_effect = '<http://tcm.lifescience.ntu.edu.tw/Effect>'
 predicate_effect = '<http://tcm.lifescience.ntu.edu.tw/effect>'
+rdfs_label = '<http://www.w3.org/2000/01/rdf-schema#label>'
 
 #reader = csv.DictReader(infile, delimiter="\t")
 reader = csv.reader(open(infilename, "rb"))
