4730a98b238a61fd921d2f90485e39029bac5b49
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-05-15 12:23:50 +0000

    Search for TCM medicne for a disease name.

---
+++ trunk/biordf2009_query_federation_case/admedapps/searchTCMByDiseaseName/application.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview
+ * TODO doc me
+ */
+
+function initialiseApplication() {
+	
+	var logReader = new YAHOO.widget.LogReader("logger");
+	
+	log("instantiate a service for the tcm widget");
+	var service = new admed.tcm.Service("../../data/tcm");
+	
+	log("instantiate a renderer for the tcm widget");
+	var renderPane = document.getElementById("tcmWidget");
+	var renderer = new admed.tcm.Widget.DefaultRenderer();
+	renderer.setCanvas(renderPane);
+	
+	log("instantiate a tcm widget");
+	widget = new admed.tcm.Widget(service, renderer);
+	
+	log("hook form submission to widget call");
+	YAHOO.util.Event.addListener("queryForm", "submit", onFormSubmit);
+	
+	log("application loaded, showing main pane");
+	admed.mvcutils.hide(document.getElementById("loadingPane"));
+	admed.mvcutils.show(document.getElementById("applicationPane"));
+}
+
+function onFormSubmit(event) {
+	log("form submitted");
+	
+	var query = document.getElementById("queryInput").value;
+	log("query: "+query);
+	
+	widget.findMedicinesByDiseaseName(query);
+}
+
+YAHOO.util.Event.onDOMReady(initialiseApplication);
