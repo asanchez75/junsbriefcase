7ab242a1c0abcc9ffd18d0c744d8b3f29ce173e5
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-05-15 12:24:50 +0000

    Search for TCM medicne for a disease name.

--- trunk/biordf2009_query_federation_case/alzheimer-drug/searchbydiseasename/tcm-service.js
+++ trunk/biordf2009_query_federation_case/alzheimer-drug/searchbydiseasename/tcm-service.js
@@ -20,46 +20,46 @@ admed.tcm.Service.prototype = new admed.sparql.Service();
 /**
  * TODO doc me
  */
-admed.tcm.Service.prototype.findGenesByDiseaseName = function( diseaseName, success, failure ) {
-    var _context = "admed.tcm.Service.prototype.findGenesByDiseaseName";
+admed.tcm.Service.prototype.findMedicineByDiseaseName = function( diseaseName, success, failure ) {
+    var _context = "admed.tcm.Service.prototype.findMedicineByDiseaseName";
 	try {
 		admed.info("diseaseName: "+diseaseName, _context);
-        var successChain = admed.chain(admed.tcm.Service.responseToGenes, success);	
-		var query = admed.tcm.Service._buildQueryForFindGenesByDiseaseName(diseaseName);
+        var successChain = admed.chain(admed.tcm.Service.responseToMedicine, success);	
+		var query = admed.tcm.Service._buildQueryForFindMedicineByDiseaseName(diseaseName);
 		this.query(query, successChain, failure);
 	}catch (error) {
         throw new admed.UnexpectedException(_context, error);
     }
 };
 
-admed.tcm.Service.responseToGenes = function( response ) {
-    var _context = "admed.tcm.Service.responseToGenes";
+admed.tcm.Service.responseToMedicine = function( response ) {
+    var _context = "admed.tcm.Service.responseToMedicine";
     try {
         admed.debug("response status: "+response.status, _context);
         admed.debug("try parsing response text as json", _context);
         admed.debug("response text: "+response.responseText, _context);
         var resultSet = YAHOO.lang.JSON.parse(response.responseText);
-        admed.debug("convert result set to an array of genes", _context);
-        var genes = admed.tcm.Gene.newInstancesFromSPARQLResults(resultSet);
-        return genes;
+        admed.debug("convert result set to an array of medicine", _context);
+        var medicines = admed.tcm.Medicine.newInstancesFromSPARQLResults(resultSet);
+        return medicines;
     } catch (e) {
         admed.debug("caught "+e.name+", "+e.message, _context);
         throw new admed.UnexpectedException(_context, e);
     }
 };
 
-admed.tcm.Service._buildQueryForFindGenesByDiseaseName = function( diseaseName ) {
+admed.tcm.Service._buildQueryForFindMedicineByDiseaseName = function( diseaseName ) {
 
 	try {
-		var prefixes = 	"PREFIX tcm: <http://purl.org/net/tcm/schema/> " +
+		var prefixes = 	"PREFIX tcm: <http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/> " +
 						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
 						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
 						
-		var body = 		"SELECT DISTINCT ?gene ?entrezgene  WHERE { " +
-							"?disease rdfs:label \"" + diseaseName + "\";" +
-								"tcm:gene ?gene . " +
-							"?gene owl:sameAs ?entrezgene . "
-						"}";
+		var body = 		"SELECT DISTINCT ?disease ?medicine ?medicinename ?ingredient ?effect WHERE { " +
+							"?disease rdfs:label ?diseasename . " +
+							" filter regex(?diseasename, \"^" + diseaseName + "\")." +
+							"?medicine tcm:treatment ?disease ; rdfs:label ?medicinename ; tcm:ingredient ?ingredient ; tcm:effect ?effect ." +
+						"}limit 7000";
 							
 		var query = prefixes + body;
 	
@@ -69,61 +69,34 @@ admed.tcm.Service._buildQueryForFindGenesByDiseaseName = function( diseaseName )
     }
 };
 
-admed.tcm.Service._buildQueryForFindImagesByFlybaseGeneID = function( flybaseGeneID ) {
-	try {
-		var prefixes = 	"PREFIX tcm: <http://purl.org/net/tcm/schema/> " +
-						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ";
-						"PREFIX so: <http://purl.org/obo/owl/SO#> ";
-						
-		var body = 		"SELECT DISTINCT ?fullImageURL ?thumbnailURL ?tcmURL ?caption ?probe ?probeLabel WHERE { " +
-							"?probe tcm:hybridisesTranscriptOf <http://openflydata.org/id/flybase/feature/" + flybaseGeneID + "> ;" +
-								"rdfs:label ?probeLabel ." +
-							"?fullImageURL " +
-								"tcm:probe ?probe ; " +
-								"tcm:thumbnail ?thumbnailURL; " +
-								"rdfs:seeAlso ?tcmURL; " +
-								"rdfs:label ?caption ." +
-						"}";
-							
-		var query = prefixes + body;
-	
-		return query;
-	}catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.Service._buildQueryForFindImagesByFlybaseGeneID", error);
-    }
-};
 
 /**
  * TDO doc me
  */
-admed.tcm.Image = function () {
+admed.tcm.Medicine = function () {
+	
+	this.fullmedicineURL = null;
 	
 	/**
 	 * @type String
 	 */
-	this.fullImageURL = null;
-	
+	this.name = null;
+		
 	/**
 	 * @type String
 	 */
-	this.thumbnailURL = null;
+	this.targetDisease = null;
+
 	
 	/**
-	 * @type String
+	 * @type array
 	 */
-	 
-	this.tcmURL = null;
+	this.ingredient = null;
 	
 	/**
-	 * @type String
+	 * @type array
 	 */
-	this.caption = null;
-	
-	/**
-	 * @type String
-	 */
-	this.probe = null;
+	this.effects = null;
 	
 
 };
@@ -132,54 +105,55 @@ admed.tcm.Image = function () {
 /**
  * TODO doc me
  */
-admed.tcm.Image.newInstancesFromSPARQLResults = function(resultSet){
+admed.tcm.Medicine.newInstancesFromSPARQLResults = function(resultSet){
 	try {
+		admed.debug ("buiding query results ");
 		var bindings = resultSet.results.bindings;
-		var imagePool = new admed.tcm.ImagePool();
-		var probePool = new admed.tcm.ProbePool();
+		var medicinePool = new admed.tcm.MedicinePool();
+//		var ingredientPool = new admed.tcm.IngredientPool();
 		
 		for (var i=0; i<bindings.length; i++) {
 			var binding = bindings[i];
 			
-			var imageURI = binding.fullImageURL.value;
+			var fullmedicineURL = binding.medicine.value;
+			
+			admed.debug ("medicine  " + fullmedicineURL);
 			//TODO
-			var image = imagePool.get(imageURI);
-//			image.fullImageURL = binding.fullImageURL.value;
-			image.thumbnailURL = binding.thumbnailURL.value;
-			image.tcmURL = binding.tcmURL.value;
-			image.caption = binding.caption.value;
+			var medicine = medicinePool.get(fullmedicineURL);
+			medicine.name = binding.medicinename.value;
+			medicine.targetDisease = binding.disease.value;
 			
-			if (binding.probe){
-				var probeURI = binding.probe.value;
-				var probe = probePool.get(probeURI);
-				
-				if (!probe.probeLabels){
-					probe.probeLabels = new Array();
-				}
-	
-				if (binding.probeLabel) {
-					var probeLabel = binding.probeLabel.value;
-					admed.util.appendIfNotMember(probe.probeLabels, probeLabel);
-				}
-				image.probe = probe;
-			}		
+//			if (binding.ingredient){
+//				var ingredientURI = binding.ingredient.value;
+//				var ingredient = ingredientPool.get(ingredientURI);
+//				
+//				if (!ingredient.ingredientLabels){
+//					ingredient.ingredientLabels = new Array();
+//				}
+//	
+//				if (binding.ingredientLabel) {
+//					var ingredientLabel = binding.ingredientLabel.value;
+//					admed.util.appendIfNotMember(ingredient.ingredientLabels, ingredientLabel);
+//				}
+//				medicine.ingredient = ingredient;
+//			}		
 		}
 		
-		return imagePool.toArray();
+		return medicinePool.toArray();
 	}catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.Image.newInstancesFromSPARQLResults", error);
+        throw new admed.UnexpectedException("admed.tcm.medicine.newInstancesFromSPARQLResults", error);
     }
 };
 
 
-admed.tcm.ImagePool = function() {
+admed.tcm.MedicinePool = function() {
 	/**
 	 * @private
 	 */
 	this._pool = new Object();
 };
 
-admed.tcm.ImagePool.prototype.toArray = function() {
+admed.tcm.MedicinePool.prototype.toArray = function() {
 	try {
 		var array = new Array();
 		for (var key in this._pool) {
@@ -187,52 +161,52 @@ admed.tcm.ImagePool.prototype.toArray = function() {
 		}
 		return array;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.ImagePool.prototype.toArray", error);
+        throw new admed.UnexpectedException("admed.tcm.medicinePool.prototype.toArray", error);
     }
 };
 
-admed.tcm.ImagePool.prototype.get = function( imagepath ) {
+admed.tcm.MedicinePool.prototype.get = function( medicinepath ) {
 	
 	try {
-		var image = this._pool[imagepath];
+		var medicine = this._pool[medicinepath];
 	
-		if ( typeof image == "undefined" || !image ) {
-			image = new admed.tcm.Image();
-			image.fullImageURL = imagepath;		
-			this._pool[imagepath] = image;	
+		if ( typeof medicine == "undefined" || !medicine ) {
+			medicine = new admed.tcm.Medicine();
+			medicine.fullmedicineURL = medicinepath;		
+			this._pool[medicinepath] = medicine;	
 		}
 		
-		return image;
+		return medicine;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.ImagePool.prototype.get", error);
+        throw new admed.UnexpectedException("admed.tcm.medicinePool.prototype.get", error);
     }
 };
 
-admed.tcm.ProbePool = function() {
+admed.tcm.ingredientPool = function() {
 	/**
 	 * @private
 	 */
 	this._pool = new Object();
 };
 
-admed.tcm.ProbePool.prototype.get = function( probeURI ) {
+admed.tcm.ingredientPool.prototype.get = function( ingredientURI ) {
 	
 	try {
-		var probe = this._pool[probeURI];
+		var ingredient = this._pool[ingredientURI];
 		
-		if (!probe ) {
-			probe = new Object();
-			probe.probeURI = probeURI;
-			this._pool[probeURI] = probe;	
+		if (!ingredient ) {
+			ingredient = new Object();
+			ingredient.ingredientURI = ingredientURI;
+			this._pool[ingredientURI] = ingredient;	
 		}
 		
-		return probe;
+		return ingredient;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.ProbePool.prototype.get", error);
+        throw new admed.UnexpectedException("admed.tcm.ingredientPool.prototype.get", error);
     }
 };
 
-admed.tcm.ProbePool.prototype.toArray = function() {
+admed.tcm.ingredientPool.prototype.toArray = function() {
 	try {
 		var array = new Array();
 		for (var key in this._pool) {
@@ -240,100 +214,6 @@ admed.tcm.ProbePool.prototype.toArray = function() {
 		}
 		return array;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.tcm.ProbePool.prototype.toArray", error);
+        throw new admed.UnexpectedException("admed.tcm.ingredientPool.prototype.toArray", error);
     }
-};
-
-
-admed.tcm.Service.transformResponseForFindImagesByFlybaseGeneIDBatch = function( response ) {
-    var _context = "admed.tcm.Service.transformResponseForFindImagesByFlybaseGeneIDBatch";
-    try {
-        
-        /* Turn results of query into result object like...
-         * {
-         *   "FBgn0036925" : {
-         * 	{
-         *     "fullImageURL": { "type": "uri" , "value": "http://www.fly-ted.org/767/2/wt01.bmp" } ,
-         *	   "thumbnailURL": { "type": "uri" , "value": "http://www.fly-ted.org/767/thumbnails/2/medium.jpg" } ,
-         * 	   "tcmURL": { "type": "uri" , "value": "http://www.fly-ted.org/767/" } ,
-         *     "caption": { "type": "literal" , "value": "schuy in wt" } ,
-         * 	   "probe": { "type": "uri" , "value": "http://openflydata.org/id/tcm/probe/schuy" } ,
-         *     "probeLabel": { "type": "literal" , "value": "schuy" }
-		 *	},
-		 *  {
-         *     "fullImageURL": { "type": "uri" , "value": "http://www.fly-ted.org/1478/2/cg17736-wt.jpg" } ,
-         *     "thumbnailURL": { "type": "uri" , "value": "http://www.fly-ted.org/1478/thumbnails/2/medium.jpg" } ,
-         *     "tcmURL": { "type": "uri" , "value": "http://www.fly-ted.org/1478/" } ,
-         *     "caption": { "type": "literal" , "value": "CG17736/schuy in wt" } ,
-         *     "probe": { "type": "uri" , "value": "http://openflydata.org/id/tcm/probe/schuy" } ,
-         *      "probeLabel": { "type": "literal" , "value": "schuy" }
-      	 *	} ,
-		 *  ...
-		 * },
-         *   "FBgn0004903" : {
-         {
-         *     "fullImageURL": { "type": "uri" , "value": "http://www.fly-ted.org/2365/2/aly01.bmp" } ,
-         *	   "thumbnailURL": { "type": "uri" , "value": "http://www.fly-ted.org/2365/thumbnails/2/medium.jpg" } ,
-         * 	   "tcmURL": { "type": "uri" , "value": "http://www.fly-ted.org/2365/" } ,
-         *     "caption": { "type": "literal" , "value": "Rb97D in aly" } ,
-         * 	   "probe": { "type": "uri" , "value": "http://openflydata.org/id/tcm/probe/Rb97D" } ,
-         *     "probeLabel": { "type": "literal" , "value": "Rb97D" }
-		 *	},
-		 *  {
-         *     "fullImageURL": { "type": "uri" , "value": "http://www.fly-ted.org/2366/2/wt01.bmp" } ,
-         *     "thumbnailURL": { "type": "uri" , "value": "http://www.fly-ted.org/2366/thumbnails/2/medium.jpg" } ,
-         *     "tcmURL": { "type": "uri" , "value": "http://www.fly-ted.org/2366/" } ,
-         *     "caption": { "type": "literal" , "value": "Rb97D in wt" } ,
-         *     "probe": { "type": "uri" , "value": "http://openflydata.org/id/tcm/probe/Rb97D" } ,
-         *     "probeLabel": { "type": "literal" , "value": "Rb97D" }
-      	 *	},
-      	 *  ....
-      	 * }
-         * }
-         */
-
-        admed.debug("response status: "+response.status, _context);
-        admed.debug("try parsing response text as json", _context);
-        admed.debug("parsing response: "+response.responseText, _context);
-
-        var resultSet = YAHOO.lang.JSON.parse(response.responseText);
-        
-        admed.debug("convert result set to a map of fbgns to images indexed by fullImageURL", _context);
-        
-        var map = {};
-        var imagePool = new admed.tcm.ImagePool();
-        var bindings = resultSet.results.bindings;
-        
-        for (var i=0; i<bindings.length; i++) {
-            var binding = bindings[i];
-            var fbgn = binding["fbgn"].value;
-         
-            var imagepath = binding["fullImageURL"].value;
-            admed.debug("add image : " + imagepath + " for gene " + fbgn, _context);
-			var image = imagePool.get(imagepath);
-			
-			admed.debug("get image object: " + image.fullImageURL + " for gene " + fbgn, _context);
-			
-			image.thumbnailURL = binding["thumbnailURL"].value;
-			image.tcmURL = binding["tcmURL"].value;
-			image.caption = binding["caption"].value;
-						
-            if (typeof map[fbgn] == "undefined" || !map[fbgn]) {
-                admed.debug("initialising gene label: "+fbgn, _context);
-                map[fbgn] = {"images":[]};
-            }
-                        
-            var images = map[fbgn].images;
-                        
-            admed.util.appendIfNotMember(images, image);
-                        
-        }
-            
-        return map;
-    
-    } 
-    catch (unexpected) {
-        admed.debug("rethrowing "+unexpected.name+", "+unexpected.message, _context);
-        throw unexpected;    
-    }    
 };
