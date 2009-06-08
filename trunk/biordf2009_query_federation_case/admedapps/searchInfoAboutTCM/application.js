184c8c1122bfd87da13727c8277d9058a1ca7642
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-08 13:50:49 +0000

    Search for some info about a herb.

---
+++ trunk/biordf2009_query_federation_case/admedapps/searchInfoAboutTCM/application.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview
+ * TODO doc me
+ */
+
+function initialiseApplication() {
+	
+	var logReader = new YAHOO.widget.LogReader("logger");
+	
+	log("instantiate service for the widget");
+	var service = new admed.tcm.Service("../../alzheimer-drug/data/tcm");
+	var effectService = new admed.effecttcm.Service("../../alzheimer-drug/data/tcm");
+	var trialService = new admed.linkedct.Service("../../alzheimer-drug/data/linkedct");
+	
+	log("instantiate a renderer for the tcm widget");
+	var renderPane = document.getElementById("tcmWidget");
+	var renderer = new admed.tcm.Widget.DefaultRenderer();
+	renderer.setCanvas(renderPane);
+	
+	log("instantiate a renderer for the effect-tcm widget");
+	var effectRenderPane = document.getElementById("effectTcmWidget");
+	var effectRenderer = new admed.effecttcm.Widget.DefaultRenderer();
+	effectRenderer.setCanvas(effectRenderPane);
+		
+	log("instantiate a renderer for the clinical trial widget");
+	var trialRenderPane = document.getElementById("trialWidget");
+	var trialRenderer = new admed.linkedct.Widget.DefaultRenderer();
+	trialRenderer.setCanvas(trialRenderPane);
+	
+	log("instantiate a tcm widget");
+	widget = new admed.tcm.Widget(service, renderer);
+	
+	log("instantiate a effect-tcm widget");
+	effectWidget = new admed.effecttcm.Widget(effectService, effectRenderer);
+	
+	log("instantiate a trial widget");
+	trialWidget = new admed.linkedct.Widget(trialService, trialRenderer);
+	
+	log("hook form submission to widget call");
+	YAHOO.util.Event.addListener("queryForm", "submit", onFormSubmit);
+	
+	log("application loaded, showing main pane");
+	admed.mvcutils.hide(document.getElementById("loadingPane"));
+	admed.mvcutils.show(document.getElementById("applicationPane"));
+	}
+
+function onFormSubmit(event) {
+	log("form submitted");
+	
+	var query = document.getElementById("queryInput").value;
+	log("query: "+query);
+	
+	widget.findMedicineFromDbpedia(query);
+	effectWidget.findEffectByMedicineName(query);
+	trialWidget.findTrialsForMedicine(query);
+}
+
+YAHOO.util.Event.onDOMReady(initialiseApplication);
