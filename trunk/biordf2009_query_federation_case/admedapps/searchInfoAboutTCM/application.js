c8ec155b9b4323820763262a3527d06599ce0ced
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-10 12:03:57 +0000

    Refactor the file structure.

--- trunk/biordf2009_query_federation_case/admedapps/searchInfoAboutTCM/application.js
+++ trunk/biordf2009_query_federation_case/admedapps/searchInfoAboutTCM/application.js
@@ -3,48 +3,64 @@
  * TODO doc me
  */
 
-
-function initialiseApplication() {
-	
-	var logReader = new YAHOO.widget.LogReader("logger");
-	
+function initHerbFinder() {
 	log("instantiate service for the widget");
 	var service = new admed.herbtcm.Service("../../alzheimer-drug/data/tcm");
-	var effectService = new admed.effecttcm.Service("../../alzheimer-drug/data/tcm");
-	var trialService = new admed.linkedct.Service("../../alzheimer-drug/data/linkedct");
-	var geneService = new admed.genetcm.Service("../../alzheimer-drug/data/tcm");
-	
 	log("instantiate a renderer for the tcm widget");
 	var renderPane = document.getElementById("tcmWidget");
 	var renderer = new admed.herbtcm.Widget.DefaultRenderer();
 	renderer.setCanvas(renderPane);
 	
+	log("instantiate a tcm widget");
+	widget = new admed.herbtcm.Widget(service, renderer);	
+};
+
+function initEffectWidget(){
+	var effectService = new admed.effecttcm.Service("../../alzheimer-drug/data/tcm");
+	
 	log("instantiate a renderer for the effect-tcm widget");
 	var effectRenderPane = document.getElementById("effectTcmWidget");
 	var effectRenderer = new admed.effecttcm.Widget.DefaultRenderer();
 	effectRenderer.setCanvas(effectRenderPane);
-		
+	
+	log("instantiate a effect-tcm widget");
+	effectWidget = new admed.effecttcm.Widget(effectService, effectRenderer);
+	
+};
+
+function initTrialWidget(){
+	var trialService = new admed.linkedct.Service("../../alzheimer-drug/data/linkedct");
+			
 	log("instantiate a renderer for the clinical trial widget");
 	var trialRenderPane = document.getElementById("trialWidget");
 	var trialRenderer = new admed.linkedct.Widget.DefaultRenderer();
 	trialRenderer.setCanvas(trialRenderPane);
 	
+	log("instantiate a trial widget");
+	trialWidget = new admed.linkedct.Widget(trialService, trialRenderer);
+};
+
+
+function initGeneWidget(){
+	var geneService = new admed.genetcm.Service("../../alzheimer-drug/data/tcm");
 	log("instantiate a renderer for the gene widget");
 	var geneRenderPane = document.getElementById("geneWidget");
 	var geneRenderer = new admed.genetcm.Widget.DefaultRenderer();
 	geneRenderer.setCanvas(geneRenderPane);
 	
-	log("instantiate a tcm widget");
-	widget = new admed.herbtcm.Widget(service, renderer);
-	
-	log("instantiate a effect-tcm widget");
-	effectWidget = new admed.effecttcm.Widget(effectService, effectRenderer);
-	
-	log("instantiate a trial widget");
-	trialWidget = new admed.linkedct.Widget(trialService, trialRenderer);
-	
 	log("instantiate a gene widget");
 	geneWidget = new admed.genetcm.Widget(geneService, geneRenderer);
+};
+
+function initialiseApplication() {
+	
+	var logReader = new YAHOO.widget.LogReader("logger");
+	
+	initHerbFinder();
+	
+	initEffectWidget();
+	
+	initTrialWidget();
 	
 	widget.subscribe("HERBSFOUND", onHerbsFound, null);
 	
@@ -63,9 +79,6 @@ function onFormSubmit(event) {
 	log("query: "+query);
 	
 	widget.findMedicineFromDbpedia(query);
-//	effectWidget.findEffectByMedicineName(query);
-//	trialWidget.findTrialsForMedicine(query);
-//	geneWidget.findGenesAssociatedWithMedicine(query);
 }
 
 function onHerbsFound(type, args){
