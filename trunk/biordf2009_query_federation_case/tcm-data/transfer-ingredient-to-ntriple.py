56307b13acccc1c61834dce35d4ad9275f45de75
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-12 13:05:58 +0000

    Revised the statistics modelling.

--- trunk/biordf2009_query_federation_case/tcm-data/transfer-ingredient-to-ntriple.py
+++ trunk/biordf2009_query_federation_case/tcm-data/transfer-ingredient-to-ntriple.py
@@ -8,11 +8,13 @@ import logging
 
 # TODO set this to file path
 #infilename = '/root/workspace/biordf2009_query_federation_case/TCM_gene_disease_associations.tab'
-infilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_ingredient_associations.tab'
+#infilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_ingredient_associations.tab'
+infilename = '\\workspaces\\zhaoj\\biordf2009_query_federation_case\\tcm-data\\rawdata\\TCM_ingredient_associations.tab'
 infile = codecs.open(infilename, mode='r', encoding='UTF-8')
 
 #outfilename = '/root/workspace/biordf2009_query_federation_case/tcm-data/TCM_gene_disease_associations.ttl'
-outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_ingredient_associations_statistics.ttl'
+#outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_ingredient_associations_statistics.ttl'
+outfilename = '\\workspaces\\zhaoj\\biordf2009_query_federation_case\\tcm-data\\TCM_ingredient_associations_statistics.ttl'
 outfile = codecs.open(outfilename, mode='w', encoding='UTF-8')
 
 namespace = "@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .\n"
@@ -31,31 +33,29 @@ rdfs_label = '<http://www.w3.org/2000/01/rdf-schema#label>'
 
 #reader = csv.DictReader(infile, delimiter="\t")
 reader = csv.reader(open(infilename, "rb"), delimiter='\t')
+#skip the first row
+reader.next();
 
-def replace_space (string):
-    return 
-
-i = X
+i = 9199
 for row in reader:
     #print "to see what is in a row: " + row[0] + "\n"
     triple = ""
-    if i > 0:
-        
-        medicineid, ingredientid, tvalue = row[0].replace(" ", "_"), re.compile('(,| )').sub("_", row[1]), row[len(row)-1].strip()
-        
-        triple = medicinename + medicineid + ">\ta\t" + type_medicine + " ;\n"
-        triple = triple + "\t" + rdfs_label + "\t\"" + row[0] + "\" ;\n"
-        triple = triple + "\t" + predicate_ingredient + "\t" +  ingredientname + ingredientid + "> .\n"
-        triple = triple + ingredientname + ingredientid + ">\ta\t" + type_ingredient + " ;\n"
-        triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" .\n"
-        
-        # updated model for describing statistics
-        triple = triple + statistic + str(i) + "\ta\t" + type_statistic + " ;\n"
-        triple = triple + "\t" + predicate_source + "\t" + medicinename + medicineid + "> ;\n"
-        triple = triple + "\t" + predicate_source + "\t" + ingredientname + ingredientid + "> ;\n"
-        triple = triple + "\t" + predicate_tvalue + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
-        
-        #triple = triple + medicinename + medicineid + ">\t" + t_value + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
+       
+    medicineid, ingredientid, tvalue = row[0].replace(" ", "_"), re.compile('(,| )').sub("_", row[1]), row[len(row)-1].strip()
+    
+    triple = medicinename + medicineid + ">\ta\t" + type_medicine + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[0] + "\" ;\n"
+    triple = triple + "\t" + predicate_ingredient + "\t" +  ingredientname + ingredientid + "> .\n"
+    triple = triple + ingredientname + ingredientid + ">\ta\t" + type_ingredient + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" .\n"
+    
+    # updated model for describing statistics
+    triple = triple + statistics + str(i) + ">\ta\t" + type_statistics + " ;\n"
+    triple = triple + "\t" + predicate_source + "\t" + medicinename + medicineid + "> ;\n"
+    triple = triple + "\t" + predicate_source + "\t" + ingredientname + ingredientid + "> ;\n"
+    triple = triple + "\t" + predicate_tvalue + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
+    
+    #triple = triple + medicinename + medicineid + ">\t" + t_value + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
     outfile.write(triple)
     outfile.flush()
     i = i + 1
