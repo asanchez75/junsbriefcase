79d67842405ec6be1d768d879f5960aeace176d6
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-12 13:02:45 +0000

    Revised the statistics modelling.

--- biordf2009_query_federation_case/tcm-data/transfer-gene-to-ntriple.py
+++ biordf2009_query_federation_case/tcm-data/transfer-gene-to-ntriple.py
@@ -8,11 +8,13 @@ import logging
 
 # TODO set this to file path
 #infilename = '/root/workspace/biordf2009_query_federation_case/TCM_gene_associations.tab'
-infilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_gene_associations.tab'
+#infilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_gene_associations.tab'
+infilename = '\\workspaces\\zhaoj\\biordf2009_query_federation_case\\tcm-data\\rawdata\\TCM_gene_associations.tab'
 infile = codecs.open(infilename, mode='r', encoding='UTF-8')
 
 #outfilename = '/root/workspace/biordf2009_query_federation_case/tcm-data/TCM_gene_associations.ttl'
-outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_gene_associations_statistics.ttl'
+#outfilename = '\\oxford\\svn\\biordf2009_query_federation_case\\tcm-data\\TCM_gene_associations_statistics.ttl'
+outfilename = '\\workspaces\\zhaoj\\biordf2009_query_federation_case\\tcm-data\\TCM_gene_associations_statistics.ttl'
 outfile = codecs.open(outfilename, mode='w', encoding='UTF-8')
 
 namespace = "@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> .\n"
@@ -31,29 +33,31 @@ rdfs_label = '<http://www.w3.org/2000/01/rdf-schema#label>'
 
 #reader = csv.DictReader(infile, delimiter="\t")
 reader = csv.reader(open(infilename, "rb"), delimiter='\t')
+#skip the first row
+reader.next();
 
 def replace_space (string):
     return 
 
-i = XX
+i = 5885
 for row in reader:
     #print "to see what is in a row: " + row[0] + "\n"
     triple = ""
-    if i > 0:
-        #print "to see what is the first column in a row: " + splits[0] + "\n"
-        medicineid, geneid, tvalue = row[0].replace(" ", "_"), row[1].replace(" ", "_"), row[len(row)-1].strip()
-        
-        triple = medicinename + medicineid + ">\ta\t" + type_medicine + " ;\n"
-        triple = triple + "\t" + rdfs_label + "\t\"" + row[0] + "\";\n"
-        triple = triple + "\t" + predicate_association + "\t" +  genename + geneid + "> .\n"
-        triple = triple + genename + geneid + ">\ta\t" + type_gene + " ;\n"
-        triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" .\n"
-        
-        # updated model for describing statistics
-        triple = triple + statistic + str(i) + "\ta\t" + type_statistic + " ;\n"
-        triple = triple + "\t" + predicate_source + "\t" + medicinename + medicineid + "> ;\n"
-        triple = triple + "\t" + predicate_source + "\t" + genename + geneid + "> ;\n"
-        triple = triple + "\t" + predicate_tvalue + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
+    
+    #print "to see what is the first column in a row: " + splits[0] + "\n"
+    medicineid, geneid, tvalue = row[0].replace(" ", "_"), row[1].replace(" ", "_"), row[len(row)-1].strip()
+    
+    triple = medicinename + medicineid + ">\ta\t" + type_medicine + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[0] + "\";\n"
+    triple = triple + "\t" + predicate_association + "\t" +  genename + geneid + "> .\n"
+    triple = triple + genename + geneid + ">\ta\t" + type_gene + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" .\n"
+    
+    # updated model for describing statistics
+    triple = triple + statistics + str(i) + ">\ta\t" + type_statistics + " ;\n"
+    triple = triple + "\t" + predicate_source + "\t" + medicinename + medicineid + "> ;\n"
+    triple = triple + "\t" + predicate_source + "\t" + genename + geneid + "> ;\n"
+    triple = triple + "\t" + predicate_tvalue + "\t\"" + tvalue + "\"^^xsd:float .\n\n"
         
         #triple = triple + medicinename + medicineid + ">\t" + t_value + "\t\"" + tvalue + "\"^^xsd:float .\n\n"        
     outfile.write(triple)
