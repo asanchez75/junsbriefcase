67b2dfc335ba4f58b7cb988d7a00da7a3641f541
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-12 15:26:31 +0000

    Revised the statistics modelling and the medicine and gene association.

--- trunk/biordf2009_query_federation_case/tcm-data/transfer-to-ntriple.py
+++ trunk/biordf2009_query_federation_case/tcm-data/transfer-to-ntriple.py
@@ -24,14 +24,15 @@ medicinename = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/medicine/
 genename = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/gene/'
 diseasename = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/disease/'
 statistics = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/statistics/'
+association = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/association/'
 type_disease = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/Disease>'
 type_medicine = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/Medicine>'
 type_gene = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/Gene>'
 type_statistics = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/Statistics>'
+type_association = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/Association>'
 predicate_medicine = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/medicine>'
 predicate_gene = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/gene>'
 predicate_contextAssociation = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/contextAssociation>'
-predicate_diseaseAssociation = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/diseaseAssociation>'
 predicate_source = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/source>'
 predicate_tvalue = '<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/medicine_gene_disease_association_tvalue>'
 rdfs_label = '<http://www.w3.org/2000/01/rdf-schema#label>'
@@ -43,23 +44,26 @@ reader = csv.reader(open(infilename, "rb"), delimiter='\t')
 reader.next();
 
 i = 19087
+j = 1
 for row in reader:
     #print "to see what is in a row: " + row[0] + "\n"
     triple = ""
     
     medicineid, geneid, diseaseid, tvalue = row[0].replace(" ", "_"), row[1].replace(" ", "_"), re.compile('(, | )').sub("_", row[2]), row[len(row)-1].strip()
     
-    triple = diseasename + diseaseid + ">\ta\t" + type_disease + " ;\n"
-    triple = triple + "\t" + rdfs_label + "\t\"" + row[2] + "\";\n"
-    triple = triple + "\t" + predicate_medicine + "\t" +  medicinename + medicineid + "> ;\n"
-    triple = triple + "\t" + predicate_gene + "\t" +  genename + geneid + "> .\n"
+    triple = association + str(j) + ">\ta\t" + type_association + " ;\n"
+    triple = triple + "\t" + predicate_contextAssociation + "\t" + medicinename + medicineid + "> ;\n"
+    triple = triple + "\t" + predicate_contextAssociation + "\t" + diseasename + diseaseid + "> ;\n"
+    triple = triple + "\t" + predicate_contextAssociation + "\t" + genename + geneid + "> .\n"
+    
+    triple = triple + diseasename + diseaseid + ">\ta\t" + type_disease + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[2] + "\".\n"
+    
     triple = triple + medicinename + medicineid + ">\ta\t" + type_medicine + " ;\n"
     triple = triple + "\t" + rdfs_label + "\t\"" + row[0] + "\" .\n"
-    triple = triple + genename + geneid + ">\ta\t" + type_gene + " ;\n"
-    triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" ;\n"
-    triple = triple + "\t" + predicate_contextAssociation + "\t" +  medicinename + medicineid + "> ;\n"
-    triple = triple + "\t" + predicate_diseaseAssociation + "\t" +  diseasename + diseaseid + "> .\n"
     
+    triple = triple + genename + geneid + ">\ta\t" + type_gene + " ;\n"
+    triple = triple + "\t" + rdfs_label + "\t\"" + row[1] + "\" .\n"
     
     # updated model for describing statistics
     triple = triple + statistics + str(i) + ">\ta\t" + type_statistics + " ;\n"
@@ -72,6 +76,7 @@ for row in reader:
     outfile.write(triple)
     outfile.flush()
     i = i + 1
+    j = j + 1
     
 
 infile.close()
