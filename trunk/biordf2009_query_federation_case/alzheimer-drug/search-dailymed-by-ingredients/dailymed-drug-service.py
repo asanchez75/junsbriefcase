09bb32609bbacba6acbecc44a43541b1cdb94348
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-18 14:02:26 +0000

    Test code of the sparql query service.

--- trunk/biordf2009_query_federation_case/alzheimer-drug/search-dailymed-by-ingredients/dailymed-drug-service.py
+++ trunk/biordf2009_query_federation_case/alzheimer-drug/search-dailymed-by-ingredients/dailymed-drug-service.py
@@ -27,15 +27,6 @@ def sparql(host, path, query):
         conn.close()
         return resultSet
 
-#query = """
-#PREFIX sc: <http://purl.org/science/owl/sciencecommons/>
-#select *
-#from <http://purl.org/science/graph/ncbi/gene-info>
-#where {
-#    ?gene sc:ggp_has_symbol "F3" .
-#    ?gene sc:ggp_from_species_described_by  <http://purl.org/commons/record/ncbi_taxonomy/9606> .
-#}"""
-
 queryHerbs = ["http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/id/medicine/Ginkgo_biloba"]
 
 queryTCMForIngredients_part1 = """
@@ -74,22 +65,7 @@ select distinct ?drug
 where {     
     ?drug rdfs:label ?drugname . filter regex(?drugname, "^"""
 
-mappingSider_part2 = """\", "i"). }LIMIT 100"""
-
-#resultset = sparql("www4.wiwiss.fu-berlin.de", "/diseasome/sparql", queryDiseasesomeForGenes)
-#    
-#if (len(resultset["results"]["bindings"])>0):
-#    for binding in resultset["results"]["bindings"]:
-#        gene = binding["gene"]["value"]
-#        print gene
-#        
-#resultset = sparql("www4.wiwiss.fu-berlin.de", "/diseasome/sparql", queryDiseasesomeForDrugs)
-#if (len(resultset["results"]["bindings"])>0):
-#    for binding in resultset["results"]["bindings"]:
-#        drug = binding["drug"]["value"]
-#        print drug
-
-#fp = open("ginkgo_side_effect.ttl", "wb")  
+mappingSider_part2 = """\", "i"). }LIMIT 100""" 
 
 outfilename = 'ginkgo_side_effect.n3'
 outfile = codecs.open(outfilename, mode='w', encoding='UTF-8')
@@ -97,66 +73,87 @@ outfile = codecs.open(outfilename, mode='w', encoding='UTF-8')
 for herb in queryHerbs:
     queryTCMForIngredients = queryTCMForIngredients_part1 + herb + queryTCMForIngredients_part2
     resultset = sparql("naxos.zoo.ox.ac.uk", "/sparql", queryTCMForIngredients)
+    
     if (len(resultset["results"]["bindings"])>0):
         for binding in resultset["results"]["bindings"]:
             ingredient = binding["ingredient"]["value"]
             dailymedIngredient = binding["dailymedIngredient"]["value"]
             
-            #### query for drugs which contain a particular active ingredient from dailymed
+            #### find dailymed drugs
             
             queryDailymedForDrug = queryDailymedForDrug_part1 + dailymedIngredient + queryDailymedForDrug_part2
                         
             dailymedResultset =  sparql("www4.wiwiss.fu-berlin.de", "/dailymed/sparql", queryDailymedForDrug)
             
+            mappedDrug = []
+            siderDrug = []
+            drugnames = {}
+            
             if (len(dailymedResultset["results"]["bindings"])>0):
+                triple = "<" + herb + ">" + "\t<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/ingredient>\t" + "<" + dailymedIngredient + "> .\n"
+                outfile.write(triple)
+                outfile.flush()
+                    
                 for binding in dailymedResultset["results"]["bindings"]:
 #                    print "start downloading drug from dailymed "
                     drug = binding["drug"]["value"]
                     drugname = binding["drugname"]["value"]
                     print drug
                     
-                    triple = "<" + herb + ">" + "\t<http://purl.org/net/tcm/tcm.lifescience.ntu.edu.tw/ingredient>\t" + "<" + dailymedIngredient + "> .\n"
-                    outfile.write(triple)
-                    outfile.flush();
-                    
                     ### find the mapping sider drug
                     
                     mappingSider = mappingSider_part1 + drugname + mappingSider_part2
-                    
                     print mappingSider
-                    
                     mappingSiderResultset = sparql("www4.wiwiss.fu-berlin.de", "/sider/sparql", mappingSider)
                     
                     if (len(mappingSiderResultset["results"]["bindings"])>0):
                         for binding in mappingSiderResultset["results"]["bindings"]:
                             drug = binding["drug"]["value"]
                             print "found " + drug
-                            triple = "<" + drug + ">" + "\t<http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/activeIngredient>\t<" + dailymedIngredient + "> ;"
-                            triple += "\t<http://www.w3.org/2000/01/rdf-schema#label>\t\"" + drugname + "\" . \n"
+                            if (drug in mappedDrug):
+                                print "do nothing"
+                            else:
+                                print "add a new drug to the array"
+                                mappedDrug.append(drug)
+                                drugnames[drug] = drugname
+                            if (drug in siderDrug):
+                                print "do nothing"
+                            else:
+                                print "add a new drug to the array"
+                                siderDrug.append(drug)
+                            
+                            
+                    else:
+                        if (drug in mappedDrug):
+                            print "do nothing"
+                        else:
+                            print "add a new drug to the array"
+                            mappedDrug.append(drug)
+                            drugnames[drug] = drugname
+                        print "no mapping for " + drug
+                
+                ## write the drug to triples
+                for drug in mappedDrug:        
+                    triple = "<" + drug + ">" + "\t<http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/activeIngredient>\t<" + dailymedIngredient + "> .\n"
+                    triple += "<" + drug + ">" + "\t<http://www.w3.org/2000/01/rdf-schema#label>\t\"" + drugnames[drug] + "\" . \n"
+                    outfile.write(triple)
+                    outfile.flush()
+                    
+                ## find side-effects
+                
+                for siderdrug in siderDrug:                                
+                    querySider = querySider_part1 + siderdrug + querySider_part2           
+                    print querySider            
+                    siderResultSet = sparql("www4.wiwiss.fu-berlin.de", "/sider/sparql", querySider)#            
+                    if (len(siderResultSet["results"]["bindings"])>0):
+                        print "found "
+                        print len(siderResultSet["results"]["bindings"])
+                        print "effects"
+                        for binding in siderResultSet["results"]["bindings"]:
+                            effect = binding["effect"]["value"]
+                            effectname = binding["effectname"]["value"]
+                            triple = "<" + siderdrug + ">" + "\t<http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect>\t<" + effect + "> .\n"
+                            triple += "<" + effect + ">\t<http://www.w3.org/2000/01/rdf-schema#label>\t\"" + effectname + "\" . \n"
                             outfile.write(triple)
                             outfile.flush()
-                            
-                            querySider = querySider_part1 + drug + querySider_part2
-                            
-                            #print querySider
-                            
-                            siderResultSet = sparql("www4.wiwiss.fu-berlin.de", "/sider/sparql", querySider)
-                            
-                            if (len(siderResultSet["results"]["bindings"])>0):
-                                print "found "
-                                print len(siderResultSet["results"]["bindings"])
-                                print "effects"
-                                for binding in siderResultSet["results"]["bindings"]:
-                                    effect = binding["effect"]["value"]
-                                    effectname = binding["effectname"]["value"]
-                                    triple = "<" + drug + ">" + "\t<http://www4.wiwiss.fu-berlin.de/sider/resource/sider/sideEffect>\t<" + effect + "> .\n"
-                                    triple += "<" + effect + ">\t<http://www.w3.org/2000/01/rdf-schema#label>\t\"" + effectname + "\" . \n"
-                                    outfile.write(triple)
-                                    outfile.flush()
-                    else:
-                        print "no mapping for " + drug
-                        triple = "<" + drug + ">" + "\t<http://www4.wiwiss.fu-berlin.de/dailymed/resource/dailymed/activeIngredient>\t<" + dailymedIngredient + "> ;"
-                        triple += "\t<http://www.w3.org/2000/01/rdf-schema#label>\t\"" + drugname + "\" . \n"
-                        outfile.write(triple)
-                        outfile.flush()
 outfile.close()    
