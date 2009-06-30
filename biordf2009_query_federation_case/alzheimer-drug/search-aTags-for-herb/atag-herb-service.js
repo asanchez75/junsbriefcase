be0999c96802ff19d4600d8ae81146042669b945
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-06-30 16:02:14 +0000

    the atags service.

--- biordf2009_query_federation_case/alzheimer-drug/search-aTags-for-herb/atag-herb-service.js
+++ biordf2009_query_federation_case/alzheimer-drug/search-aTags-for-herb/atag-herb-service.js
@@ -1,11 +1,11 @@
 // create a namespace if not already defined
-admed.namespace("admed.dbherb");
+admed.namespace("admed.atags");
 
 
 /**
  * TODO doc me
  */
-admed.dbherb.Service = function (endpointURL) {
+admed.atags.Service = function (endpointURL) {
 	/**
 	 * @private
 	 */
@@ -14,56 +14,55 @@ admed.dbherb.Service = function (endpointURL) {
 
 
 // extend
-admed.dbherb.Service.prototype = new admed.sparql.Service();
+admed.atags.Service.prototype = new admed.sparql.Service();
 
 
 /**
  * TODO doc me
  */
-admed.dbherb.Service.prototype.findMedicineFromDbpedia = function( medicineName, success, failure ) {
-    var _context = "admed.dbherb.Service.prototype.findMedicineFromDbpedia";
+admed.atags.Service.prototype.findMedicineFromAtags = function( dbpediaHerbURI, success, failure ) {
+    var _context = "admed.atags.Service.prototype.findMedicineFromAtags";
 	try {
-		admed.info("medicineName: "+medicineName, _context);
-        var successChain = admed.chain(admed.dbherb.Service.responseToMedicine, success);	
-		var query = admed.dbherb.Service._buildQueryForFindMedicineFromDbpedia(medicineName);
+		admed.info("herbURI: "+dbpediaHerbURI, _context);
+        var successChain = admed.chain(admed.atags.Service.responsToAtags, success);	
+		var query = admed.atags.Service._buildQueryForFindMedicineFromAtags(dbpediaHerbURI);
 		this.query(query, successChain, failure);
 	}catch (error) {
         throw new admed.UnexpectedException(_context, error);
     }
 };
 
-admed.dbherb.Service.responseToMedicine = function( response ) {
-    var _context = "admed.dbherb.Service.responseToMedicine";
+admed.atags.Service.responsToAtags = function( response ) {
+    var _context = "admed.atags.Service.responseToMedicine";
     try {
         admed.debug("response status: "+response.status, _context);
         admed.debug("try parsing response text as json", _context);
         admed.debug("response text: "+response.responseText, _context);
         var resultSet = YAHOO.lang.JSON.parse(response.responseText);
-        admed.debug("convert result set to an array of medicine", _context);
-        var medicines = admed.dbherb.Medicine.newInstancesFromSPARQLResults(resultSet);
-        return medicines;
+        admed.debug("convert result set to an array of atags", _context);
+        var atags = admed.atags.Atags.newInstancesFromSPARQLResults(resultSet);
+        return atags;
     } catch (e) {
         admed.debug("caught "+e.name+", "+e.message, _context);
         throw new admed.UnexpectedException(_context, e);
     }
 };
 
-admed.dbherb.Service._buildQueryForFindMedicineFromDbpedia = function( medicine ) {
+admed.atags.Service._buildQueryForFindMedicineFromAtags = function( dbpediaHerbURI ) {
 
 	try {
 		var prefixes = 	"PREFIX sioc: <http://rdfs.org/sioc/ns#>  ";
 						
 		var body = 		"SELECT DISTINCT ?atag ?topic ?content where { " +
-							" dbpedia:binomial_authority ?authority; dbpedia:classis ?classis; dbpedia:division ?division; " +
-										"dbpedia:family ?family; dbpedia:genus ?genus; dbpedia:kingdom ?kingdom; dbpedia:order ?order; dbpedia:species ?species ;"+
-										"foaf:img ?img ." +
-						"}limit 100";
+							"?atag sioc:topic <" + dbpediaHerbURI +
+							"> ; sioc:topic ?topic; sioc:content ?content ." +
+						"}limit 300";
 							
 		var query = prefixes + body;
 	
 		return query;
 	}catch (error) {
-        throw new admed.UnexpectedException("admed.dbherb.Service._buildQueryForFindMedicineFromDbpedia", error);
+        throw new admed.UnexpectedException("admed.atags.Service._buildQueryForFindMedicineFromAtags", error);
     }
 };
 
@@ -71,28 +70,14 @@ admed.dbherb.Service._buildQueryForFindMedicineFromDbpedia = function( medicine 
 /**
  * TDO doc me
  */
-admed.dbherb.Medicine = function () {
+admed.atags.Atags = function () {
 	
 	/**
 	 * @type String
 	 */
-	this.authority = null;
+	this.topic = new Array();
 	
-	this.classis = null;
-	
-	this.division = null;
-	
-	this.family = null;
-	
-	this.genus = null;
-	
-	this.kingdom = null;
-	
-	this.order = null;
-	
-	this.species = null;
-	
-	this.img = null;
+	this.content = null;
 
 };
 
@@ -100,46 +85,45 @@ admed.dbherb.Medicine = function () {
 /**
  * TODO doc me
  */
-admed.dbherb.Medicine.newInstancesFromSPARQLResults = function(resultSet){
+admed.atags.Atags.newInstancesFromSPARQLResults = function(resultSet){
 	try {
 		admed.debug ("buiding query results ");
 		var bindings = resultSet.results.bindings;
-		var medicinePool = new admed.dbherb.MedicinePool();
-//		var ingredientPool = new admed.dbherb.IngredientPool();
+		var atagsPool = new admed.atags.AtagsPool();
+//		var ingredientPool = new admed.atags.IngredientPool();
 		
 		for (var i=0; i<bindings.length; i++) {
 			var binding = bindings[i];
 			
-			var imgURL = binding.img.value;
+			var aTagURI = binding.atag.value;
 			
-			var medicine = medicinePool.get(imgURL);
-			 
-			medicine.authority = binding.authority.value;
-			medicine.classis = binding.classis.value;
-			medicine.division = binding.division.value;
-			medicine.family = binding.family.value;
-			medicine.genus = binding.genus.value;
-			medicine.kingdom = binding.kingdom.value;
-			medicine.order = binding.order.value;
-			medicine.species = binding.species.value;
-			medicine.img = binding.img.value;
+			var atag = atagsPool.get(aTagURI);			 
+			
+			if (!atag.content)
+				atag.content = binding.content.value;
+			
+			if (binding.topic){
+				topics = atag.topic;
+				admed.util.appendIfNotMember(topics, binding.topic.value);				
+				atag.topic = topics;
+			}	
 		}
 		
-		return medicinePool.toArray();
+		return atagsPool.toArray();
 	}catch (error) {
-        throw new admed.UnexpectedException("admed.dbherb.medicine.newInstancesFromSPARQLResults", error);
+        throw new admed.UnexpectedException("admed.atags.Atags.newInstancesFromSPARQLResults", error);
     }
 };
 
 
-admed.dbherb.MedicinePool = function() {
+admed.atags.AtagsPool = function() {
 	/**
 	 * @private
 	 */
 	this._pool = new Object();
 };
 
-admed.dbherb.MedicinePool.prototype.toArray = function() {
+admed.atags.AtagsPool.prototype.toArray = function() {
 	try {
 		var array = new Array();
 		for (var key in this._pool) {
@@ -147,23 +131,23 @@ admed.dbherb.MedicinePool.prototype.toArray = function() {
 		}
 		return array;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.dbherb.medicinePool.prototype.toArray", error);
+        throw new admed.UnexpectedException("admed.atags.AtagsPool.prototype.toArray", error);
     }
 };
 
-admed.dbherb.MedicinePool.prototype.get = function( medicinepath ) {
+admed.atags.AtagsPool.prototype.get = function( aTagURI ) {
 	
 	try {
-		var medicine = this._pool[medicinepath];
+		var atag = this._pool[aTagURI];
 	
-		if ( typeof medicine == "undefined" || !medicine ) {
-			medicine = new admed.dbherb.Medicine();
-			//medicine.fullmedicineURL = medicinepath;		
-			this._pool[medicinepath] = medicine;	
+		if ( typeof atag == "undefined" || !atag ) {
+			atag = new admed.atags.Atags();
+					
+			this._pool[aTagURI] = atag;	
 		}
 		
-		return medicine;
+		return atag;
 	} catch (error) {
-        throw new admed.UnexpectedException("admed.dbherb.medicinePool.prototype.get", error);
+        throw new admed.UnexpectedException("admed.atags.AtagsPool.prototype.get", error);
     }
 };
