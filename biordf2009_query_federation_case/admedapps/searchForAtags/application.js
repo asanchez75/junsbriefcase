fcaef3638ace534e9bcbaae2d49964da458a00f5
jun.zhao@zoo.ox.ac.uk <jun.zhao@zoo.ox.ac.uk@74b91ec8-edf5-11dd-b787-91885b911a96>
2009-07-01 09:10:35 +0000

    Atags application searching by herb name.

--- biordf2009_query_federation_case/admedapps/searchForAtags/application.js
+++ biordf2009_query_federation_case/admedapps/searchForAtags/application.js
@@ -1,3 +1,14 @@
+function initHerbFinder() {
+	log("instantiate service for the widget");
+	var service = new admed.herbtcm.Service("../../alzheimer-drug/data/tcm");
+	log("instantiate a renderer for the tcm widget");
+	var renderPane = document.getElementById("tcmWidget");
+	var renderer = new admed.herbtcm.Widget.DefaultRenderer();
+	renderer.setCanvas(renderPane);
+	
+	log("instantiate a tcm widget");
+	widget = new admed.herbtcm.Widget(service, renderer);	
+};
 
 function initAtagWidget(){
 	log("instantiate service for the atags widget");
@@ -19,10 +30,13 @@ function initialiseApplication() {
 	
 	var logReader = new YAHOO.widget.LogReader("logger");
 	
+	initHerbFinder();
 	initAtagWidget();
 	
 	log("hook form submission to widget call");
+	widget.subscribe("HERBSFOUND", onHerbsFound, null);
 	YAHOO.util.Event.addListener("queryForm", "submit", onFormSubmit);
+	
 	
 	log("application loaded, showing main pane");
 	admed.mvcutils.hide(document.getElementById("loadingPane"));
@@ -35,7 +49,18 @@ function onFormSubmit(event) {
 	var query = document.getElementById("queryInput").value;
 	log("query: "+query);
 	
-	atagsWidget.findAtagsForMedicine(query);
+
+	widget.findMedicineFromDbpedia(query);
+}
+
+function onHerbsFound(type, args){
+	var herbs = args[0];
+	log ("Find herb " + herbs[0].fullmedicineURL);
+	herb = 	herbs[0].fullmedicineURL;
+	
+	var dbherb = herbs[0].herbFromDbpedia;
+	atagsWidget.findAtagsForMedicine(dbherb);
+	
 }
 
 
